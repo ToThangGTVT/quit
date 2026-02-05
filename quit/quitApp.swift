7ff1500730afda398aea10ae4145b96a4aa6cae0
ThangTQ <tothang97nbvn@gmail.com>
2026-02-05 20:59:58 +0700

    Initial Commit

---
+++ quit/quitApp.swift
@@ -0,0 +1,17 @@
+//
+//  quitApp.swift
+//  quit
+//
+//  Created by thắng tô on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct quitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
